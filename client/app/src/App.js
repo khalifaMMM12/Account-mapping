1a085e43a0b69dec5fa190898dfa883647e79dad
Mudathir M Muhammad <khalifasitemsn@gmail.com>
2023-12-13 12:56:45 +0100

    commit

--- client/app/src/App.js
+++ client/app/src/App.js
@@ -4,9 +4,7 @@ import Navbar from './components/navbar.js';
 import Login from './Pages/login-page.js';
 import Officer from './Pages/accountOfficer';
 import Sidebar from './components/sidebar.js';
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min";
-import "bootstrap-icons/font/bootstrap-icons.css";
+
 
 function App() {
   return (
