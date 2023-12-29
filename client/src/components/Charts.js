cd2260370cb61d491ef524e58b34d841894cf79b
Mudathir M Muhammad <khalifasitemsn@gmail.com>
2023-12-29 10:05:15 +0100

    update

--- client/src/components/Charts.js
+++ client/src/components/Charts.js
@@ -2,35 +2,31 @@
 import React from "react";
 import Chartist from "react-chartist";
 import ChartistTooltip from 'chartist-plugin-tooltips-updated';
+import Chart from 'chart.js'
 
 export const AUMChart = () => {
-  const data = {
-    labels: ['Zone', 'Branch', 'General'],
-    series: [[1, 3, 6,]]
-  };
+  var income = document.getElementById("income").getContext("2d");
+  new Chart(income).Bar(barData);
 
-  const options = {
-    low: 0,
-    showArea: true,
-    fullWidth: true,
-    axisX: {
-      position: 'end',
-      showGrid: true
-    },
-    axisY: {
-      // On the y-axis start means left and end means right
-      showGrid: false,
-      showLabel: false,
-      labelInterpolationFnc: value => `$${value / 1}k`
-    }
-  };
-
-  const plugins = [
-    ChartistTooltip()
-  ]
+  var barData = {
+    labels : ["January","February","March","April","May","June"],
+    datasets : [
+      {
+        fillColor : "#48A497",
+        strokeColor : "#48A4D1",
+        data : [456,479,324,569,702,600]
+      },
+      {
+        fillColor : "rgba(73,188,170,0.4)",
+        strokeColor : "rgba(72,174,209,0.4)",
+        data : [364,504,605,400,345,320]
+      }
+  
+    ]
+  }
 
   return (
-    <Chartist data={data} options={{...options, plugins}} type="Bar" className="ct-series-g ct-double-octave" />
+    <canvas id="income" width="600" height="400"></canvas>
   );
 };
 
